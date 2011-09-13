0947160d46c9350275b92dc1a7389d12d77bd361
superbrothers <superbrothers@shisobu.in>
2011-09-14 00:24:13 +0900

    create new project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipSampleApp::Application.config.secret_token = '71c5caffcd263112ccbca39a0521af91d2ae5f59fc38d28e5850716a0b47a46b6accbacec88a698ddf898811862a8f986c339899dd0cd0e00c29de4a4835096a'
