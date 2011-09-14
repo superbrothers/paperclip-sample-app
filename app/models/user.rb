dae13dfcc5bdfea2053818ecbf1b4973d39dfebc
superbrothers <superbrothers@shisobu.in>
2011-09-14 12:18:05 +0900

    small fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
                              :url => '/assets/:attachment/:id/:style/:basename.:extension',
                              :path => ':rails_root/public/assets/:attachment/:id/:style/:basename.:extension'
 
-  validates_attachment_content_type :avatar, :content_type => /^image\/(:?p?jpe?g|(:?x-)?png|gif)/
+  validates_attachment_content_type :avatar, :content_type => /^image\/(:?p?jpe?g|(:?x-)?png|gif)$/
   validates_attachment_size :avatar, :less_than => 2.megabytes
 end
